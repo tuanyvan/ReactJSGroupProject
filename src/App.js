a784f657e448194aceed3814e94ec4cfeec07d91
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2023-04-02 17:52:40 -0400

    Create functional component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import "./App.css";
+
+function FormComponent() {
+  return (
+    <div className="d-flex flex-row p-4">
+      <div className="col-8">
+        <div className="col-12">
+          <label className="col-3" for="name">
+            Name
+          </label>
+          <input type="text" id="name" name="name"></input>
+        </div>
+        <div className="col-12">
+          <label className="col-3" for="number">
+            Number
+          </label>
+          <button className="btn btn-primary">-</button>
+          <input type="number" id="number" name="number" disabled></input>
+          <button className="btn btn-primary">+</button>
+        </div>
+      </div>
+      <div className="col-4 border"></div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <FormComponent />
     </div>
   );
 }
