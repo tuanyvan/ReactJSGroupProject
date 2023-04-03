18d246c54dc4b997c824c81e126da2ae6f939a1a
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2023-04-03 17:53:56 -0400

    Demonstrate React Router

---
+++ react-demo/src/Welcome.tsx
@@ -0,0 +1,9 @@
+
+export default function Welcome() {
+    return (
+        <div className="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
+            <h1>Welcome to the React-Demo!</h1>
+            <a href="/app" className="btn btn-success">Click me to go to the app.</a>
+        </div>
+    )
+}
--- react-demo/src/index.tsx
+++ react-demo/src/index.tsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Welcome from './Welcome';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Welcome />}></Route>
+          <Route path="app" element={<App />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
