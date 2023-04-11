3e5ab553a965e8eb2470ed62bacee2644a611b50
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2023-04-11 19:00:18 -0400

    React router complete

---
+++ vite-react-demo/src/Welcome.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+    return (
+        <div>
+            <h1>Welcome to our React DEMO!</h1>
+            <a href="/app" className="btn btn-success">Click me to go the app.</a>
+        </div>
+    )
+}
--- vite-react-demo/src/main.tsx
+++ vite-react-demo/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Welcome from './Welcome'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Welcome />}></Route>
+          <Route path="app" element={<App />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
